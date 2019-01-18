c11299618e98cd750583df021578eab2379e7fe8
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-18 14:19:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
